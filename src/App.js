fffb8a4f1e7f24888d2c33b36f0256491101bf48
Bamiji Oyetubo <60369677+bigboybamo@users.noreply.github.com>
2020-10-22 13:28:21 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import "./App.css";
+import FetchUser2 from "./components/FetchUser2";
+
+class App extends Component {
+  state = {
+    visible: true,
+  };
+
+  render() {
+    return (
+      <div className="App">
+        This is working
+        <FetchUser2 />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/FetchUser2.js
@@ -0,0 +1,47 @@
+import React from "react";
+import "../App.css";
+import { Card } from "react-bootstrap";
+
+export default class FetchUser extends React.Component {
+  constructor() {
+    super();
+    this.state = { data: [] };
+  }
+
+  async componentDidMount() {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
+    const json = await response.json();
+    this.setState({ data: json });
+  }
+
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.state.data.map((el) => (
+            <div key={el.id}>
+              <Card style={{ width: "18rem" }}>
+                <Card.Img variant="top" />
+                <Card.Body>
+                  <Card.Title>
+                    <h1 className="contactName">{el.name[0]}</h1>
+                  </Card.Title>
+                  <Card.Text>
+                    <strong>{el.name}</strong>
+                  </Card.Text>
+                  <Card.Text>{el.username}</Card.Text>
+                  <Card.Text>
+                    <a href="#">
+                      <u>{el.email}</u>
+                    </a>
+                  </Card.Text>
+                  <button className="btnCard">MORE DETAILS</button>
+                </Card.Body>
+              </Card>
+            </div>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
